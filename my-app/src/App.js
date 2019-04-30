dea6b730d7d4183cebdc449fce43596d0d08d1b5
maryamf93 <maryamfooladi21@yahoo.com>
2019-04-30 04:47:39 +0430

    change style forms

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Forms/Login';
+import SignUp from './Components/Forms/SignUp';
+import Messenger from './Components/Messenger/Messenger';
 
 class App extends React.Component {
   render() {
     return (
-        <Login />  
+        <Messenger />  
     );
   }
 }
--- my-app/src/Components/Forms/Login.js
+++ my-app/src/Components/Forms/Login.js
@@ -1,26 +1,50 @@
 import React from 'react'; 
 import Input from './Input';
+import logo from '../../logo.svg'
 
 class Login extends React.Component {
-    
+    constructor(props) {
+        super(props)
+        this.state= {
+            email: '',
+            password: ''
+        }
+    }
+
+    onChange = (event) =>{
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({ [name] : value },
+            () => {
+               this.usernameValidator(this.state.username);
+               this.passwordValidator(this.state.password);
+            }
+         );
+     }
+     
     render() {
         return (
             <div className='container' >
                 <h1>
                     Log in
                 </h1>
-                <img className='logo' />
+                <img className='logo' src={logo} />
                 <input 
                     className='input-box' 
                     type='email' 
+                    name='email'
                     placeholder='Email'
-                    value={this.props.email}
+                    value={this.state.email}
+                    onChange={this.onChange}
                 />
                 <input 
                     className='input-box' 
                     type='password' 
+                    name='password'
                     placeholder='password'
-                    value={this.props.password}
+                    value={this.state.password}
+                    onChange={this.onChange}
+                    
                 />
                 <button className='submit input-box' >
                     Log in
